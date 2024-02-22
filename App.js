85aaa6900d14bdfc366e7d73f7481382068bb597
Spartan334 <ianmina0322@gmail.com>
2024-02-22 17:27:21 +0800

    first

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>111019022 陳奕誠 數位三甲</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f4f',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    color: '#12d',
+    fontSize: 30,
+  }
+
 });
